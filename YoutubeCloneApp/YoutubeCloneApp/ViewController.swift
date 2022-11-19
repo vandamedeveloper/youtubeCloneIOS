55931d591f1fa978fc99ee7e10d38e49ae59faac
Vandame <vandamedeveloper@gmail.com>
2022-11-19 14:08:50 +0100

    created app

---
+++ YoutubeCloneApp/YoutubeCloneApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YoutubeCloneApp
+//
+//  Created by Vandi Hrustic on 19/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
